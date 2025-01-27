65945fd18544a2547aa58e438c1379e3f1451b2d
yasraa <yyasrakashif@gmail.com>
2025-01-27 04:01:48 -0500

    Initial commit

---
+++ vite-project/tailwind.config.js
@@ -0,0 +1,14 @@
+
+module.exports = {
+ 
+  content: [
+    "./index.html",
+    "./src/**/*.{html,js,ts,jsx,tsx}", // Include HTML files in src as well
+  ],
+  
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
